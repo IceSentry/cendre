d38c4b579ac4b293d5bf9a367d4e9fd4225de787
IceSentry <c.giguere42@gmail.com>
2023-04-04 21:14:49 -0400

    setup bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use bevy::{
+    a11y::AccessibilityPlugin, app::AppExit, input::InputPlugin, prelude::*, winit::WinitPlugin,
+};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(MinimalPlugins)
+        .add_plugin(WindowPlugin {
+            primary_window: Some(Window {
+                title: "cendre".into(),
+                ..default()
+            }),
+            ..default()
+        })
+        .add_plugin(AccessibilityPlugin)
+        .add_plugin(WinitPlugin)
+        .add_plugin(InputPlugin)
+        .add_system(exit_on_esc)
+        .run();
+}
+
+fn exit_on_esc(key_input: Res<Input<KeyCode>>, mut exit_events: EventWriter<AppExit>) {
+    if key_input.just_pressed(KeyCode::Escape) {
+        exit_events.send_default();
+    }
 }
